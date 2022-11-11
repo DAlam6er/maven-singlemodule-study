513104b0a3ba9cf20cb49ae89e0729d856150e48
Anton Pashkin <anton.zep@gmail.com>
2022-11-11 20:58:00 +0300

    added example source classes

---
+++ src/main/java/com/dmdev/servlet/FirstServlet.java
@@ -0,0 +1,10 @@
+package com.dmdev.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet
+{
+
+}
